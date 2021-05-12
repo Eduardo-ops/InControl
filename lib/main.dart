c5c718c003810bcc1dc130c72f9c222088f51573
Eduardo Isidoro Gonçalves <eduardoi@fuvs.br>
2021-05-12 11:48:26 -0300

    Botoes adicionados

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,29 @@ void main() {
       home: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text("Pessoas: 0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
           Text(
-            "Disponível !",
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                  child: Text(
+                    "+1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {}),
+              FlatButton(
+                  child: Text(
+                    "-1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {}),
+            ],
+          ),
+          Text(
+            "Disponível!",
             style: TextStyle(
                 color: Colors.green,
                 fontStyle: FontStyle.italic,
